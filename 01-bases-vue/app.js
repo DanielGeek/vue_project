9cdbdd7b826d7698ab4af2935fb9133e801e805c
DangelGlobant <daniel30081990@gmail.com>
2022-04-28 12:35:28 -0400

    32. Component Status - Data

--- 01-bases-vue/app.js
+++ 01-bases-vue/app.js
@@ -3,6 +3,13 @@ const app = Vue.createApp({
   // <h1> Hola mundo</h1>
   // <p> Desde app.js </p>
   // `
+
+  data() {
+    return {
+      quote: "I'm Batman",
+      author: 'Bruce Wayne'
+    }
+  }
 })
 
 app.mount('#myApp')
