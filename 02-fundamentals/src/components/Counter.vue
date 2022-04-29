e659e0ebf71e56237b187274cac31abcfcceba05
DangelGlobant <daniel30081990@gmail.com>
2022-04-29 12:47:33 -0400

    50. Increase and Decrease

--- 02-fundamentals/src/components/Counter.vue
+++ 02-fundamentals/src/components/Counter.vue
@@ -1,6 +1,12 @@
 <template>
   <h2>Counter!!!!!!!!</h2>
   <p> {{ counter }} <sup>2</sup> = {{ counter * counter }}</p>
+
+  <div>
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
+  </div>
+
 </template>
 
 <script>
@@ -14,7 +20,13 @@ export default {
   methods: {
     getSquareValue() {
       return this.counter * this.counter
-    }
+    },
+    increase() {
+      this.counter++
+    },
+    decrease() {
+      this.counter--
+    },
   },
   computed: {
     squareCounter() {
@@ -26,5 +38,21 @@ export default {
 </script>
 
 <style>
+
+button{
+  background-color: #64BB87;
+  border-radius: 5px;
+  border: 1px solid white;
+  color: white;
+  cursor: pointer;
+  margin: 0 5px;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
+}
+
+button:hover {
+  background-color: #5aa67b;
+  transition: 0.3s ease-in-out;
+}
 
 </style>
