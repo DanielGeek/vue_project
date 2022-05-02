e09e4acc3886d5ff42a17f5e401d75118d1516fa
DangelGlobant <daniel30081990@gmail.com>
2022-05-01 23:06:54 -0400

    68. Expect

--- 02-fundamentals/tests/unit/example.spec.js
+++ 02-fundamentals/tests/unit/example.spec.js
@@ -3,17 +3,13 @@ describe('Example Component', () => {
   test('Must be greater than 10', () => {
 
     // Arrange
-    let value = 5;
+    let value = 10;
 
     // Act
     value = value + 2
 
     // Assert
-    if( value > 10 ) {
-
-    } else {
-      throw `${ value } no es mayor a 10`
-    }
+    expect( value ).toBeGreaterThan( 10 );
   })
 
 })
